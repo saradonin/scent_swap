146c4e8505db951a5f7e46202b5200e188e47e7c
saradonin <m.jemieljanczuk@gmail.com>
2023-08-12 21:45:40 +0200

    search POST action added

--- scent_app/views.py
+++ scent_app/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 from django.views import View
 
+from scent_app.forms import SearchForm
+from scent_app.models import Brand, Perfume, Category, User
+
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, 'index.html', )
+        form = SearchForm()
+        return render(request, 'index.html', {'form': form})
+
+    def post(self, request):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search_value = form.cleaned_data['value']
+            matched_brands = Brand.objects.filter(name__icontains=search_value)
+            matched_perfumes = Perfume.objects.filter(name__icontains=search_value)
+            matched_offers = Perfume.objects.filter(requested_perfume__name__icontains=search_value)
+            matched_users = User.objects.filter(username__icontains=search_value)
+            if not matched_products.exists() and not matched_categories.exists():
+                message = "Sorry, nothing to show you"
+            else:
+                message = ''
+
+            ctx = {
+                'form': form,
+                'brands': matched_brands,
+                'perfumes': matched_perfumes,
+                'offers': matched_offers,
+                'users': matched_users,
+                'message': message
+            }
+            return render(request, 'search.html', ctx)
