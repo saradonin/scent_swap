b818b0812e9d9834cc383b4640aada1e9d80d96b
saradonin <m.jemieljanczuk@gmail.com>
2023-08-13 21:24:45 +0200

    minor form tweaks

--- scent_app/forms.py
+++ scent_app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class SearchForm(forms.Form):
     value = forms.CharField(label="",
-                           max_length=32,
-                           widget=forms.TextInput(attrs={'placeholder': 'Search',
-                                                         'class': 'form-control bg-secondary',
-                                                         'style': 'width: 160px;'}))
+                            max_length=32,
+                            widget=forms.TextInput(attrs={'placeholder': 'Search',
+                                                          'class': 'form-control bg-secondary text-white',
+                                                          'style': 'width: 160px;'}))
