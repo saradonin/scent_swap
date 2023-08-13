b64e5410ee6216665cf71e60f7592f2f831aec98
saradonin <m.jemieljanczuk@gmail.com>
2023-08-13 22:44:27 +0200

    model updated

--- scent_app/models.py
+++ scent_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
     description = models.TextField(null=True)
 
 
