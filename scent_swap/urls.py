d715a0d84e70c4b9a312d4584ff2fda2bbf2ff00
saradonin <m.jemieljanczuk@gmail.com>
2023-08-13 21:24:51 +0200

    search url added

--- scent_swap/urls.py
+++ scent_swap/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from scent_app.views import IndexView
+from scent_app.views import IndexView, SearchView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name="index"),
+    path('search', SearchView.as_view(), name="search"),
 ]
