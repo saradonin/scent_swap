31f39e1d3c2f3665240142472beaaf8fc46ce3e1
saradonin <m.jemieljanczuk@gmail.com>
2023-08-16 21:03:06 +0200

    populate tweaks

--- scent_app/management/commands/populateperfumes.py
+++ scent_app/management/commands/populateperfumes.py
@@ -7,6 +7,6 @@ class Command(BaseCommand):
     help = 'Add perfumes'
 
     def handle(self, *args, **options):
-        for _ in range(200):
+        for _ in range(500):
             generate_random_perfume()
         self.stdout.write(self.style.SUCCESS("Random fake perfumes added"))
