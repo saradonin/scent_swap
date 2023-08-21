5cbb73cb7d063fc659de56734cc38c4a36532f02
saradonin <m.jemieljanczuk@gmail.com>
2023-08-21 22:52:28 +0200

    docstring added to forms and validator

--- scent_app/forms.py
+++ scent_app/forms.py
@@ -8,8 +8,7 @@ class SearchForm(forms.Form):
     value = forms.CharField(label="",
                             max_length=32,
                             widget=forms.TextInput(attrs={'placeholder': 'Search',
-                                                          'class': 'form-control bg-secondary text-white',
-                                                          'style': 'width: 160px;'}))
+                                                          'class': 'form-control bg-secondary text-white'}))
 
 
 class UserLoginForm(forms.Form):
@@ -18,12 +17,27 @@ class UserLoginForm(forms.Form):
 
 
 class UserAddForm(forms.Form):
+    """
+    Form for adding a new user.
+
+    Attributes:
+        username (str): The username of the new user, limited to 64 characters.
+        email (str): The email address of the new user, limited to 64 characters.
+        password1 (str): The password of the new user.
+        password2 (str): The repeated password for confirmation.
+    """
     username = forms.CharField(max_length=64, validators=[validate_username_unique])
     email = forms.CharField(max_length=64, validators=[validate_email])
     password1 = forms.CharField(max_length=64, label="Password:", widget=forms.PasswordInput)
     password2 = forms.CharField(max_length=64, label="Repeat password:", widget=forms.PasswordInput)
 
     def clean(self):
+        """
+        Clean and validate the form data.
+
+        Raises:
+            forms.ValidationError: If the passwords do not match.
+        """
         cleaned_data = super().clean()
         password1 = cleaned_data.get('password1')
         password2 = cleaned_data.get('password2')
--- scent_app/validators.py
+++ scent_app/validators.py
@@ -3,5 +3,8 @@ from django.contrib.auth.models import User
 
 
 def validate_username_unique(value):
+    """
+    Validate whether username is unique
+    """
     if User.objects.filter(username=value):
         raise forms.ValidationError("Username is already taken")
